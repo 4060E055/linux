ea88f255da8cdafbcc48694a95164a2a01469202
4060E055 <37571816+4060E055@users.noreply.github.com>
2020-11-08 23:46:52 +0800

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -10,4 +10,10 @@ iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -s 172.19.155.0/24 -j ACCEPT
 iptables -A INPUT –s 172.18.255.0/24 –j ACCEPT
 iptables -A INPUT -t tcp --dport 80 -j ACCEPT
+
+iptables -t nat -F
+iptables -t nat -X
+iptables -t nat -Z 
+iptables -t nat -A POSTROUTING -s 172.19.155.1/24 -o ens3 -j MASQUERADE
+
 iptables-save > /etc/sysconfig/iptables
