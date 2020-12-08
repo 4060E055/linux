e4ee73c73a47b19ede162dcece9a0c2768ee75ca
4060E055 <37571816+4060E055@users.noreply.github.com>
2020-12-08 20:24:35 +0800

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -9,7 +9,7 @@ iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT 
 iptables -A INPUT -s 172.19.155.0/24 -j ACCEPT
 iptables -A INPUT –s 172.18.255.0/24 –j ACCEPT
-iptables -A INPUT -t tcp --dport 80 -j ACCEPT
+iptables -A INPUT - tcp --dport 80 -j ACCEPT
 iptables -A INPUT -s 172.18.255.155/24 -p tcp --dport 3131 -j ACCEPT
 iptables -A INPUT –s 172.19.155.254/24 -p tcp --dport 3131 -j ACCEPT
 iptables -A INPUT -s 10.10.10.0/24 -p tcp --dport 3131 -j REJECT
